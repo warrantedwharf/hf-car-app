bf06fe2de04ee8a07292e561bf03b135ddeff959
warrantedwharf <abeljohn@outlook.com>
2019-10-17 17:12:25 +0530

    program for extracting data offchain integrated

--- fab-net/server.js
+++ fab-net/server.js
@@ -2,6 +2,9 @@
 // to make a server
 const http = require('http');
 
+// import child_process to spawn blockEventlistener
+const spawn = require('child_process').spawn;
+
 // import from app.js
 // Autommatically looks for js files
 const app = require('./app');
@@ -17,6 +20,22 @@ const port = process.env.PORT || 4000;
 // (app) is passed so the express app handles the
 // request
 const server = http.createServer(app);
+
+// path to blockEventlistener program
+const pgm_bEl = process.cwd() + '/child_process/offchaindb/blockEventListener';
+
+// spawn the blockEventListener program
+const proc = spawn(process.argv0, [pgm_bEl]);
+
+// log events to catch output/errors
+proc.stdout.on('data', function(data){
+    console.log(data.toString());
+});
+
+// log errors from blockEventListener
+proc.stderr.on('error', function(error){
+    console.log(error.toString());
+});
 
 // This starts the server and listens at the port
 server.listen(port);
